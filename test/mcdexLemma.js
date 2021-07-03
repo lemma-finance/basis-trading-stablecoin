b760c8efbb0d6ea341adb1f9d81db1a5bf6a037a
yashnaman <shahyashs145@gmail.com>
2021-07-03 19:37:43 +0530

    test for seeing how the funding rate changes

--- test/mcdexLemma.js
+++ test/mcdexLemma.js
@@ -1,7 +1,7 @@
 const { JsonRpcProvider } = require('@ethersproject/providers');
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
-const { CHAIN_ID_TO_POOL_CREATOR_ADDRESS, PoolCreatorFactory, ReaderFactory, LiquidityPoolFactory, IERC20Factory, CHAIN_ID_TO_READER_ADDRESS } = require('@mcdex/mai3.js');
+const { CHAIN_ID_TO_POOL_CREATOR_ADDRESS, PoolCreatorFactory, ReaderFactory, LiquidityPoolFactory, IERC20Factory, CHAIN_ID_TO_READER_ADDRESS, getLiquidityPool } = require('@mcdex/mai3.js');
 const { utils } = require('ethers');
 const { BigNumber, constants } = ethers;
 const notifier = require('node-notifier');
@@ -16,7 +16,7 @@ const arbProvider = new JsonRpcProvider('https://kovan.infura.io/v3/2a1a54c3aa37
 describe("mcdexLemma", function () {
     let usdLemma, reInvestor, hasWETH;
     let liquidityPool, reader;
-    const perpetualIndex = "0"; //in Kovan the 0th perp for 0th liquidity pool = inverse ETH-USD
+    const perpetualIndex = 0; //in Kovan the 0th perp for 0th liquidity pool = inverse ETH-USD
     const provider = ethers.provider;
     const ZERO = BigNumber.from("0");
     beforeEach(async function () {
@@ -55,11 +55,54 @@ describe("mcdexLemma", function () {
         const balanceOfHasWETH = await this.collateral.balanceOf("0x5FD7d6382De0D4c4A00B19Ed10c11dfD96C27340");
         console.log("balance of hasWETH", balanceOfHasWETH.toString());
 
+        {
+            const accountsResult = await reader.callStatic.getAccountStorage(liquidityPool.address, perpetualIndex, this.mcdexLemma.address);
+            const liquidityPoolInfo = await getLiquidityPool(reader, liquidityPool.address);
+            const perpetualInfo = liquidityPoolInfo.perpetuals.get(perpetualIndex);
+
+            console.log("fundingRate", perpetualInfo.fundingRate.toString());
+            console.log("unitAccumulativeFunding", perpetualInfo.unitAccumulativeFunding.toString());
+
+            ///need to use .callStatic as to explicitly tell to node that it is not a state changing transaction and return the result (We have to do this because the reader contract implements this method without making it a "view" method even though it is)
+
+            console.log(accountsResult.toString());
+        }
+
     });
     // it("should initialize correctly", async function () {
     //     expect(await this.mcdexLemma.usdLemma()).to.equal(usdLemma.address);
     // });
-    it("should open position correctly", async function () {
+    // it("should open and close position correctly", async function () {
+    //     const trader = this.mcdexLemma.address;
+    //     //find an address with the WETH 
+    //     const amount = utils.parseUnits("1", "18");
+    //     //The WETH on kovan deployment of MCDEX has 18 decimals
+    //     console.log("amount", amount.toString());
+    //     console.log("collateral decimals", (await this.collateral.decimals()).toString());
+
+    //     await this.collateral.connect(hasWETH).transfer(this.mcdexLemma.address, amount);
+    //     const amountInUSD = utils.parseUnits("2000", "18");
+    //     await this.mcdexLemma.open(amountInUSD);
+
+    //     await liquidityPool.forceToSyncState();
+
+    //     const amountInUSDClose = utils.parseUnits("1990", "18");
+    //     await this.mcdexLemma.close(amountInUSDClose);
+
+    //     // const accountsResult = await reader.callStatic.getAccountStorage(liquidityPool.address, perpetualIndex, this.mcdexLemma.address);
+    //     //need to use .callStatic as to explicitly tell to node that it is not a state changing transaction and return the result (We have to do this because the reader contract implements this method without making it a "view" method even though it is)
+
+    //     // console.log(accountsResult);
+    //     // console.log(accountsResult.toString());
+
+    //     // const marginAccount = await liquidityPool.getMarginAccount(perpetualIndex, trader);
+    //     // console.log(marginAccount);
+    //     // console.log(marginAccount.toString());
+    //     // String
+    //     notifier.notify('Test done');
+    // });
+
+    it("check how the funding rate changes things", async function () {
         const trader = this.mcdexLemma.address;
         //find an address with the WETH 
         const amount = utils.parseUnits("1", "18");
@@ -73,19 +116,43 @@ describe("mcdexLemma", function () {
 
         await liquidityPool.forceToSyncState();
 
-        const amountInUSDClose = utils.parseUnits("1990", "18");
-        await this.mcdexLemma.close(amountInUSDClose);
+        {
+            const accountsResult = await reader.callStatic.getAccountStorage(liquidityPool.address, perpetualIndex, this.mcdexLemma.address);
+            const liquidityPoolInfo = await getLiquidityPool(reader, liquidityPool.address);
+            const perpetualInfo = liquidityPoolInfo.perpetuals.get(perpetualIndex);
 
-        // const accountsResult = await reader.callStatic.getAccountStorage(liquidityPool.address, perpetualIndex, this.mcdexLemma.address);
-        //need to use .callStatic as to explicitly tell to node that it is not a state changing transaction and return the result (We have to do this because the reader contract implements this method without making it a "view" method even though it is)
+            console.log("fundingRate", perpetualInfo.fundingRate.toString());
+            console.log("unitAccumulativeFunding", perpetualInfo.unitAccumulativeFunding.toString());
 
-        // console.log(accountsResult);
-        // console.log(accountsResult.toString());
+            ///need to use .callStatic as to explicitly tell to node that it is not a state changing transaction and return the result (We have to do this because the reader contract implements this method without making it a "view" method even though it is)
 
-        // const marginAccount = await liquidityPool.getMarginAccount(perpetualIndex, trader);
-        // console.log(marginAccount);
-        // console.log(marginAccount.toString());
-        // String
-        notifier.notify('Test done');
+            console.log(accountsResult.toString());
+        }
+
+        //go forward in time to be able to distribute the fundingPayments
+        await hre.network.provider.request({
+            method: "evm_increaseTime",
+            params: [60 * 60 * 9] //8 hours
+        }
+        );
+        await hre.network.provider.request({
+            method: "evm_mine",
+        }
+        );
+
+        ///update the cumulative funding Rate
+        await liquidityPool.forceToSyncState();
+        {
+            const accountsResult = await reader.callStatic.getAccountStorage(liquidityPool.address, perpetualIndex, this.mcdexLemma.address);
+            const liquidityPoolInfo = await getLiquidityPool(reader, liquidityPool.address);
+            const perpetualInfo = liquidityPoolInfo.perpetuals.get(perpetualIndex);
+
+            console.log("fundingRate", perpetualInfo.fundingRate.toString());
+            console.log("unitAccumulativeFunding", perpetualInfo.unitAccumulativeFunding.toString());
+
+            ///need to use .callStatic as to explicitly tell to node that it is not a state changing transaction and return the result (We have to do this because the reader contract implements this method without making it a "view" method even though it is)
+
+            console.log(accountsResult.toString());
+        }
     });
 });
