8d89aab63c93943eaa2bf176ec71def06d067d93
Ankit Parashar <ankitabc12@gmail.com>
2021-08-16 10:59:16 +0530

    adding staking changes

---
+++ contracts/interfaces/IXUSDL.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.3;
+
+import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+interface IXUSDL {
+
+    function usdl() external view returns (IERC20Upgradeable); 
+
+    function balance() external view returns (uint256);
+
+    /// @notice Minimum blocks user funds need to be locked in contract
+    /// @return Minimum blocks for which USDL will be locked
+    function MINIMUM_LOCK() external view returns (uint256);
+
+    /// @notice Deposit and mint xUSDL in exchange of USDL
+    /// @param amount of USDL to deposit
+    /// @return Amount of xUSDL minted
+    function deposit(uint256 amount) external returns (uint256);
+
+    /// @notice Withdraw USDL and burn xUSDL
+    /// @param shares of xUSDL to burn
+    /// @return Amount of USDL withdrawn
+    function withdraw(uint256 shares) external returns (uint256);
+
+    /// @notice Price per share in terms of USDL
+    /// @return Price of 1 xUSDL in terms of USDL
+    function pricePerShare() external view returns (uint256);
+
+    /// @notice Block number after which user can withdraw USDL
+    /// @return Block number after which user can withdraw USDL 
+    function userUnlockBlock(address usr) external view returns (uint256);
+
+}
---
+++ contracts/xUSDL.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.3;
+
+import { ERC20Upgradeable, IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { OwnableUpgradeable, ContextUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { ERC2771ContextUpgradeable } from "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
+import { IXUSDL } from './interfaces/IXUSDL.sol';
+
+contract xUSDL is IXUSDL, ERC20Upgradeable, OwnableUpgradeable, ERC2771ContextUpgradeable {
+
+
+    uint256 public override MINIMUM_LOCK = 100;
+
+    mapping(address => uint256) public override userUnlockBlock;
+
+    IERC20Upgradeable public override usdl;
+
+    function initialize(
+        address _trustedForwarder,
+        address _usdl
+    ) external initializer {
+        __Ownable_init();
+        __ERC20_init("xUSDLemma", "xUSDL");
+        __ERC2771Context_init(_trustedForwarder);
+        usdl = IERC20Upgradeable(_usdl);
+        usdl.approve(address(usdl), type(uint256).max);
+    }
+
+    function resetApprovals() external {
+        usdl.approve(address(usdl), type(uint256).max);
+    }
+
+    function balance() public override view returns (uint256 balance) {
+        balance = usdl.balanceOf(address(this));
+    }
+
+    function deposit(uint256 amount) external override returns (uint256 shares) {
+        usdl.transferFrom(_msgSender(), address(this), amount);
+        shares = amount / pricePerShare();
+        userUnlockBlock[_msgSender()] = block.number + MINIMUM_LOCK;
+        _mint(_msgSender(), amount);
+    }
+
+
+    function withdraw(uint256 shares) external override returns (uint256 amount) {
+        require(block.number >= userUnlockBlock[_msgSender()], "xUSDL: Locked tokens");
+        amount = pricePerShare() * shares;
+        usdl.transfer(_msgSender(), amount);
+        _burn(_msgSender(), shares);
+    }
+
+
+    function pricePerShare() public view override returns (uint256 price) {
+        price = balance() / totalSupply();
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal override {
+        require(block.number > userUnlockBlock[sender], "ERC20: tokens not yet unlocked");
+        super._transfer(sender, recipient, amount);
+    }
+
+
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(ContextUpgradeable, ERC2771ContextUpgradeable)
+        returns (address sender)
+    {
+        //ERC2771ContextUpgradeable._msgSender();
+        return super._msgSender();
+    }
+
+    function _msgData()
+        internal
+        view
+        virtual
+        override(ContextUpgradeable, ERC2771ContextUpgradeable)
+        returns (bytes calldata)
+    {
+        //ERC2771ContextUpgradeable._msgData();
+        return super._msgData();
+    }
+
+} 
