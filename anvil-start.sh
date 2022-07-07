27e9ce79403a03a94f59c4c98b8ad639e78678c1
K-Aizen <k.aizen.crypto.dev@gmail.com>
2022-07-07 16:45:26 +0000

    Adding Scripts for an Anvil Test Env where cheatcodes to get money work

--- anvil-start.sh
+++ anvil-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Run Anvil forking Optimism 
-anvil --fork-url https://opt-mainnet.g.alchemy.com/v2/j9pgL8KP33EnVCItge8fRMZPnikQaMBI --fork-block-number 12137998
+anvil --fork-url https://opt-mainnet.g.alchemy.com/v2/j9pgL8KP33EnVCItge8fRMZPnikQaMBI --fork-block-number 12137998 --block-base-fee-per-gas 0
+
