8c1bcf15636115dadca55b9e15a4dd2ba938df91
k-aizen <k.aizen.crypto.dev@gmail.com>
2022-07-19 07:10:34 +0000

    Adding Swap Example1

--- scripts/rebalancer-bot.sol
+++ scripts/rebalancer-bot.sol
@@ -4,6 +4,7 @@
 import "forge-std/Script.sol";
 import "../src/Deploy.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "../contracts/interfaces/IERC20Decimals.sol";
 
 struct ExternalContracts {
@@ -13,7 +14,11 @@ struct ExternalContracts {
     IERC20Decimals USDC;
 }
 
-contract MyScript is Script {
+
+
+
+
+contract MyScript is Script, Test {
     DeployAnvilOptimism public dao;
     ExternalContracts public ec;
 
@@ -28,6 +33,10 @@ contract MyScript is Script {
         string memory output = abi.decode(res, (string));
         console.log(output);
         // assertEq(output, "gm");
+    }
+
+    function _getMoney(address token, uint256 amount) internal {
+        deal(token, address(this), amount);
     }
 
     function _getConfig() internal {
@@ -58,10 +67,48 @@ contract MyScript is Script {
         console.log("UniV3 WETH-USDC Pool = ", pool);
     }
 
+
+    function _testUniV3Swap(address tokenIn, uint256 amountIn) internal returns(uint256 amountOut) {
+        IERC20Decimals(tokenIn).approve(ec.uniV3Router, type(uint256).max);
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(ec.USDC),
+                tokenOut: address(ec.WETH),
+                fee: 3000,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = ISwapRouter(ec.uniV3Router).exactInputSingle(params);
+        console.log("[_testUniV3Swap()] Swap Result = ", amountOut);
+    }
+
+    function _testUniV3Rebalance() internal {
+
+    }
+
     function run() external {
         console.log("Starting Script");
         // test1();
         _getConfig();
+        console.log("USDC Balance Before = ", ec.USDC.balanceOf(address(this)));
+        console.log("WETH Balance Before = ", ec.WETH.balanceOf(address(this)));
+        _getMoney(address(ec.USDC), 1e12);
+
+        console.log("USDC Balance After = ", ec.USDC.balanceOf(address(this)));
+        console.log("WETH Balance After = ", ec.WETH.balanceOf(address(this)));
+
+        console.log("Test ISDC --> WETH Swap");
+
+        _testUniV3Swap(address(ec.USDC), 1e6);
+
+        console.log("USDC Balance Swap After = ", ec.USDC.balanceOf(address(this)));
+        console.log("WETH Balance Swap After = ", ec.WETH.balanceOf(address(this)));
+
         _testUniV3Factory();
         // vm.startBroadcast();
         // dao = new DeployAnvilOptimism();
