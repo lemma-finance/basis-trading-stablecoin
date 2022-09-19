bd314cd8ab8b1bb5b2c123118e575f8d3fe27763
k-aizen <k.aizen.crypto.dev@gmail.com>
2022-09-19 08:48:05 +0000

    Moving to Solidity 0.8.4 for custom errors support

--- contracts/interfaces/IERC20Decimals.sol
+++ contracts/interfaces/IERC20Decimals.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.3;
+// pragma solidity ^0.8.3;
 
 import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
